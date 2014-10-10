c98f3362753c437755d512c1cfce5b7209b0d046
acejang1994 <byumho.jang@students.olin.edu>
2014-10-09 20:17:12 -0400

    Added changes

--- LoCo/app/src/main/java/com/example/flymperopoulos/loco/MainPageFragment.java
+++ LoCo/app/src/main/java/com/example/flymperopoulos/loco/MainPageFragment.java
@@ -1,7 +1,11 @@
 package com.example.flymperopoulos.loco;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,6 +16,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -31,9 +36,19 @@ import java.util.ArrayList;
 /**
  * Created by james on 10/9/14.
  */
-public class MainPageFragment extends Fragment {
+public class MainPageFragment extends Fragment implements LocationListener{
+
+    private LocationManager locationManager;
     private Context context;
+    private TextView latituteField;
+    private TextView longitudeField;
     public MainPageFragment(){
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        this.context = activity;
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -52,12 +67,23 @@ public class MainPageFragment extends Fragment {
         RequestQueue queue = Volley.newRequestQueue(getActivity());
 
         String url = "https://maps.googleapis.com/maps/api/geocode/";
-        String url1 = "https://maps.googleapis.com/maps/api/geocode/json?sensor=true&latlng=40.714224,-73.961452&key=AIzaSyClQMzD6V2eLi6CkqoSQzxCtarjlbcgSE4";
 
-        String key = "AIzaSyClQMzD6V2eLi6CkqoSQzxCtarjlbcgSE4";
-        String latlng= "40.714224,-73.961452";
+        String key = "AIzaSyBeknu4C9t4Dii04H8imC-ygXLvprFLfv4";
+        String url1 = "https://maps.googleapis.com/maps/api/geocode/json?sensor=true&latlng=40.714224,-73.961452&key=" + key;
 
-//        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        latituteField = (TextView) rootView.findViewById(R.id.lat);
+        longitudeField = (TextView) rootView.findViewById(R.id.longi);
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(provider);
+        locationManager.requestLocationUpdates(provider, 0, 0 , this);
+        if (location != null) {
+            onLocationChanged(location);
+        } else {
+            latituteField.setText("Location not available");
+            longitudeField.setText("Location not available");
+        }
 
         final TextView display = (TextView)rootView.findViewById(R.id.display);
 
@@ -69,15 +95,14 @@ public class MainPageFragment extends Fragment {
                     public void onResponse(JSONObject response) {
                         Log.i(response.toString(), "What is in the json\n\n\n\n");
                         try {
-                            String address = response.getJSONArray("results").get(0).toString();
-
-
-                            display.setText(address);
+                            String stuff = response.getJSONArray("results").get(1).toString();
+//                            String address = response.get("result").toString();
+                            Log.i(stuff, "What is in the json\n\n\n\n");
+                            display.setText(stuff);
 
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
-
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -88,8 +113,36 @@ public class MainPageFragment extends Fragment {
         queue.add(jsonRequest);
 
 
-
         return rootView;
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        int lat = (int) (location.getLatitude());
+        int lng = (int) (location.getLongitude());
+        latituteField.setText(String.valueOf(lat));
+        longitudeField.setText(String.valueOf(lng));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
 
     }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
 }
