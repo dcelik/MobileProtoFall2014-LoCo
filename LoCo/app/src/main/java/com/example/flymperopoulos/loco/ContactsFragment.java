aa9d3f279d8d6c158ba38609e7cfa8d04bf5b3e7
Deniz Celik <deniz.celik@students.olin.edu>
2014-10-14 19:23:44 -0400

    Added META ignores to allow for imports

--- LoCo/app/src/main/java/com/example/flymperopoulos/loco/ContactsFragment.java
+++ LoCo/app/src/main/java/com/example/flymperopoulos/loco/ContactsFragment.java
@@ -11,8 +11,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
