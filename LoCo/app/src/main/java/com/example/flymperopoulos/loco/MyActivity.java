477c606d969d54ce0ce50ad4af7c0a57c6b4fc5a
flymperopoulos <filippos.lymperopoulos@students.olin.edu>
2014-10-16 04:19:31 -0400

    Added notification alert

--- LoCo/app/src/main/java/com/example/flymperopoulos/loco/MyActivity.java
+++ LoCo/app/src/main/java/com/example/flymperopoulos/loco/MyActivity.java
@@ -3,9 +3,15 @@ package com.example.flymperopoulos.loco;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.firebase.client.Firebase;
 
@@ -14,6 +20,7 @@ import java.util.ArrayList;
 
 public class MyActivity extends Activity {
 
+    NotificationManager NM;
     HandlerDatabase db;
     Firebase fb;
     User currentUser;
@@ -86,6 +93,20 @@ public class MyActivity extends Activity {
         FragmentTransaction transaction = fm.beginTransaction();
         transaction.replace(R.id.container, fragment);
         transaction.commit();
+    }
+
+    @SuppressWarnings("deprecation")
+    public void notify(View vobj){
+        String title = "New request!";
+        String subject = " requests your location!";
+        String body = "";
+        NM=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification notify=new Notification(android.R.drawable.
+                stat_notify_more,title,System.currentTimeMillis());
+        PendingIntent pending= PendingIntent.getActivity(
+                getApplicationContext(), 0, new Intent(), 0);
+        notify.setLatestEventInfo(getApplicationContext(),subject,body,pending);
+        NM.notify(0, notify);
     }
 
 
